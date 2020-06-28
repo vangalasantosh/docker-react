75926b17b8eaec42ff3660f4e6962e9166c26094
vangalas <santosh.vangala-non-empl@moodys.com>
2020-06-28 16:20:13 -0400

    changed app.js

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          How are you 
+          How are you doing. I am Santosh
         </p>
         <a
           className="App-link"
